0ceeb9e526ed4ce1f2c95d6781a086087fe5b517
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-02-01 17:02:33 -0600

    Add Spartan font to chakra ui

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,8 +2,19 @@ import type { AppProps } from 'next/app'
 
 import { ChakraProvider } from '@chakra-ui/react'
 
+import { extendTheme } from '@chakra-ui/react'
+
+import '@fontsource/spartan/400.css'
+
+const theme = extendTheme({
+  fonts: {
+    heading: `"Spartan", sans-serif`,
+    body: `"Spartan", sans-serif`,
+  },
+})
+
 export default function App({ Component, pageProps }: AppProps) {
-  return <ChakraProvider>
+  return <ChakraProvider theme={theme}>
     <Component {...pageProps} />
   </ChakraProvider>
 }

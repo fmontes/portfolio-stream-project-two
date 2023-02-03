c6c021cbb008b2dc187cc2d63553fe705efd438e
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-02-03 17:36:13 -0600

    Wrap topbar component

--- src/components/TopBar.tsx
+++ src/components/TopBar.tsx
@@ -1,33 +1,23 @@
 import Link from "next/link";
 import Image from "next/image";
+import { Flex, Spacer } from '@chakra-ui/react'
+
 
 export function TopBar() {
-  return (<div>
-    <ul>
-      <li>
-        <Link href="#">+38 (050) 12 34 567</Link>
-      </li>
-      <li>
-        <Link href="#">Ukraine, Kyiv,Khreshchatyk 1</Link>
-      </li>
-      <li>
-        <Link href="#">All week 24/7</Link>
-      </li>
-    </ul>
+  return (<Flex bg='black' w='100%' p={2} color='white' justifyContent={"space-between"} fontSize="xs">
+    <Flex gap="1.5rem">
+      <Flex as={Link} href="#" alignItems="center" gap="0.5rem"><Image src="/ico-small-phone.svg" width={24} height="24" alt="" /> +38 (050) 12 34 567</Flex>
 
-    <ul>
-      <li>
-        <Link href="#"><Image src="/ico-small-fb.svg" width={24} height="24" alt="" /></Link>
-      </li>
-      <li>
-        <Link href="#"><Image src="/ico-small-fb.svg" width={24} height="24" alt="" /></Link>
-      </li>
-      <li>
-        <Link href="#"><Image src="/ico-small-fb.svg" width={24} height="24" alt="" /></Link>
-      </li>
-      <li>
-        <Link href="#"><Image src="/ico-small-fb.svg" width={24} height="24" alt="" /></Link>
-      </li>
-    </ul>
-  </div>);
+      <Flex as={Link} href="#" alignItems="center" gap="0.5rem"><Image src="/ico-small-location.svg" width={24} height="24" alt="" /> Ukraine, Kyiv,Khreshchatyk 1</Flex>
+
+      <Flex as={Link} href="#" alignItems="center" gap="0.5rem"><Image src="/ico-small-clock.svg" width={24} height="24" alt="" /> All week 24/7</Flex>
+    </Flex>
+
+    <Flex gap="1rem">
+      <Link href="#"><Image src="/ico-small-fb.svg" width={24} height="24" alt="" /></Link>
+      <Link href="#"><Image src="/ico-small-tw.svg" width={24} height="24" alt="" /></Link>
+      <Link href="#"><Image src="/ico-small-ig.svg" width={24} height="24" alt="" /></Link>
+      <Link href="#"><Image src="/ico-small-pin.svg" width={24} height="24" alt="" /></Link>
+    </Flex>
+  </Flex>);
 }

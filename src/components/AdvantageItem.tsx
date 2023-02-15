8e550af224c16ae96586201ca83ed2606ca1405c
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-02-15 17:24:25 -0600

    Make advantage responsive

--- src/components/AdvantageItem.tsx
+++ src/components/AdvantageItem.tsx
@@ -1,6 +1,6 @@
 import Image from "next/image";
 
-import { Grid, GridItem, Text } from "@chakra-ui/react";
+import { Grid, GridItem, Show, Text } from "@chakra-ui/react";
 
 type Props = {
   title: string;
@@ -9,13 +9,21 @@ type Props = {
 }
 
 export function AdvantageItem({title, content, icon}: Props) {
-  return (<Grid gridTemplateColumns="40px 1fr" alignItems="center" gap="1rem">
+  return (<Grid gridTemplateColumns={{
+    base: "1fr",
+    sm: '40px 1fr'
+  }} alignItems="center" justifyItems="center" gap={{
+    base: '1rem',
+    sm: '0.5rem'
+  }}>
     <GridItem>
       <Image src={icon} width={40} height={40} alt="" />
     </GridItem>
     <GridItem>
       <Text textTransform="uppercase" fontSize="xs" fontWeight="bold">{title}</Text>
-      <Text fontSize="xs">{content}</Text>
+      <Show above='sm'>
+        <Text fontSize="xs">{content}</Text>
+      </Show>
     </GridItem>
   </Grid>);
 }

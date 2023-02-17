0b1794fed028885f1e5e99a934a931870725f0fb
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-02-17 13:36:44 -0600

    Update show/hide nav

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
 import Image from "next/image";
-import { Container, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList } from '@chakra-ui/react'
+import { Container, Flex, IconButton, Menu, MenuButton, MenuItem, MenuList, Show } from '@chakra-ui/react'
 import { Box } from '@chakra-ui/react'
 import { HamburgerIcon } from "@chakra-ui/icons";
 
@@ -40,37 +40,40 @@ export function Header() {
   return (<Box w="100%" as="header" borderBottom="solid 2px" borderColor="gray.100">
     <Container as={Flex} justifyContent="space-between" alignItems="center" size="lg">
       <Flex gap={'0.5rem'} margin="1rem 0">
-        <Box as="nav" display={{ md: 'none' }}>
-          {/* TODO: find a way to remove the code duplication */}
-          <Menu>
-            <MenuButton
-              as={IconButton}
-              aria-label='Options'
-              icon={<HamburgerIcon />}
-              variant='outline'
-            />
-            <MenuList>
-              {
-                menu.map((item, index) => {
-                  return <MenuItem key={index}>
-                    <Link href={item.href}>{item.title}</Link>
-                  </MenuItem>
-                })
-              }
-            </MenuList>
-          </Menu>
-        </Box>
+        <Show below='sm'>
+          <Box as="nav">
+            <Menu>
+              <MenuButton
+                as={IconButton}
+                aria-label='Options'
+                icon={<HamburgerIcon />}
+                variant='outline'
+              />
+              <MenuList>
+                {
+                  menu.map((item, index) => {
+                    return <MenuItem key={index}>
+                      <Link href={item.href}>{item.title}</Link>
+                    </MenuItem>
+                  })
+                }
+              </MenuList>
+            </Menu>
+          </Box>
+        </Show>
         <Image src="/logo.svg" alt="" width={100} height={48} />
       </Flex>
-      <Box as="nav" display={{ base: 'none', sm: 'block' }}>
-        <Flex as="ul" listStyleType="none" gap="2rem">
-          {
-            menu.map((item, index) => {
-              return <li key={index}><Link href={item.href}>{item.title}</Link></li>
-            })
-          }
-        </Flex>
-      </Box>
+      <Show above='sm'>
+        <Box as="nav">
+          <Flex as="ul" listStyleType="none" gap="2rem">
+            {
+              menu.map((item, index) => {
+                return <li key={index}><Link href={item.href}>{item.title}</Link></li>
+              })
+            }
+          </Flex>
+        </Box>
+      </Show>
 
       <div className="commerce-menu">
         <Flex as="ul" listStyleType="none" gap="1.5rem">

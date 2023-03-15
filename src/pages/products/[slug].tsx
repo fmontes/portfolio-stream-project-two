721c01ea09cc5a020abcc696bfc3f72f4c7e607b
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-03-15 17:28:43 -0600

    Add content to the PDP

--- src/pages/products/[slug].tsx
+++ src/pages/products/[slug].tsx
@@ -1,21 +1,44 @@
+import Image from 'next/image';
+
 import { slugify } from '@/utils/sluglify';
+import { AspectRatio, Button } from '@chakra-ui/react';
 import { Product as ProductModel } from '../index';
 
 type Props = {
-  product: ProductModel
+  product: ProductModel;
 };
 
-export default function Product(props: Props) {
-  return <h1>{props.product.title}</h1>;
+export default function Product({ product: { title, price, image, rating, category, description } }: Props) {
+  return (
+    <>
+      <h1>{title}</h1>
+      <p>{price}</p>
+      <p>
+        {rating.count}, {rating.rate}
+      </p>
+      <p>{category}</p>
+      <p>{description}</p>
+      <Button>Add to Cart</Button>
+      <AspectRatio position="relative" ratio={1} maxWidth="100%" marginBottom={'1rem'}>
+        <Image
+          src={image}
+          alt=""
+          fill={true}
+          style={{
+            objectFit: 'contain',
+          }}
+        />
+      </AspectRatio>
+    </>
+  );
 }
-
 
 // Generates `/products/1` and `/products/2`
 export async function getStaticPaths() {
   const products = await fetch('https://fakestoreapi.com/products').then((res) => res.json());
 
   const slugs: string[] = products.map((product: ProductModel) => {
-    return `${slugify(product.title)}-${product.id}`
+    return `${slugify(product.title)}-${product.id}`;
   });
 
   return {
@@ -33,7 +56,7 @@ export async function getStaticProps(context: { params: { slug: string } }) {
   return {
     // Passed to the page component as props
     props: {
-      product
-    }
+      product,
+    },
   };
 }

a57ad57c664c1c9bfe1f8946d21ff37184fa0835
Freddy Montes <751424+fmontes@users.noreply.github.com>
2023-04-19 16:17:49 -0600

    Fix indent

--- src/pages/category/[category].tsx
+++ src/pages/category/[category].tsx
@@ -9,97 +9,82 @@ import { ChangeEvent, useState } from 'react';
 import { Product } from '..';
 
 type Props = {
-    products: Product[];
-    category: string;
+  products: Product[];
+  category: string;
 };
 
 type FilteredProducts = {
-    products: Product[];
-    filtered: 'IDLE' | 'FILTERED';
+  products: Product[];
+  filtered: 'IDLE' | 'FILTERED';
 };
 
 export default function Category({ products, category }: Props) {
-    const [filteredProducts, setFilteredProducts] = useState<FilteredProducts>({
+  const [filteredProducts, setFilteredProducts] = useState<FilteredProducts>({
+    products: [],
+    filtered: 'IDLE',
+  });
+
+  const rangeOptions = getPriceFilterRanges(products.map((p) => p.price));
+
+  function onFilterSelected(e: ChangeEvent<HTMLSelectElement>) {
+    const [min, max] = e.target.value.split('-');
+    if (min || max) {
+      const filtered = products.filter((p) => p.price >= parseFloat(min) && p.price <= parseFloat(max));
+
+      setFilteredProducts({
+        products: filtered,
+        filtered: 'FILTERED',
+      });
+    } else {
+      setFilteredProducts({
         products: [],
         filtered: 'IDLE',
-    });
-
-    const rangeOptions = getPriceFilterRanges(products.map((p) => p.price));
-
-    function onFilterSelected(e: ChangeEvent<HTMLSelectElement>) {
-        const [min, max] = e.target.value.split('-');
-        if (min || max) {
-            const filtered = products.filter(
-                (p) => p.price >= parseFloat(min) && p.price <= parseFloat(max)
-            );
-
-            setFilteredProducts({
-                products: filtered,
-                filtered: 'FILTERED',
-            });
-        } else {
-            setFilteredProducts({
-                products: [],
-                filtered: 'IDLE',
-            });
-        }
+      });
     }
+  }
 
-    const emptyFilter =
-        filteredProducts.filtered === 'FILTERED' &&
-        !filteredProducts.products.length;
-    const data = filteredProducts.products.length
-        ? filteredProducts.products
-        : products;
+  const emptyFilter = filteredProducts.filtered === 'FILTERED' && !filteredProducts.products.length;
+  const data = filteredProducts.products.length ? filteredProducts.products : products;
 
-    return (
-        <>
-            <HeaderSecondary
-                breadcrumb={[
-                    {
-                        href: '/',
-                        text: category,
-                    },
-                ]}
-            />
-            <Container mt={'3rem'}>
-                <Select
-                    onChange={onFilterSelected}
-                    placeholder='Select option'
-                    w='200px'
-                    mb={'2rem'}
-                >
-                    {rangeOptions.map(({ label, value: { max, min } }, key) => (
-                        <option value={`${min}-${max}`} key={key}>
-                            {label}
-                        </option>
-                    ))}
-                </Select>
-                {emptyFilter ? (
-                    <h3>No products in this filter</h3>
-                ) : (
-                    <ProductsGrid products={data} />
-                )}
-            </Container>
-        </>
-    );
+  return (
+    <>
+      <HeaderSecondary
+        breadcrumb={[
+          {
+            href: '/',
+            text: category,
+          },
+        ]}
+      />
+      <Container mt={'3rem'}>
+        <Select onChange={onFilterSelected} placeholder="Select option" w="200px" mb={'2rem'}>
+          {rangeOptions.map(({ label, value: { max, min } }, key) => (
+            <option value={`${min}-${max}`} key={key}>
+              {label}
+            </option>
+          ))}
+        </Select>
+        {emptyFilter ? <h3>No products in this filter</h3> : <ProductsGrid products={data} />}
+      </Container>
+    </>
+  );
 }
 
 export const getServerSideProps: GetServerSideProps = async (context) => {
-    const category = context.query.category;
-    const url = `https://fakestoreapi.com/products/category/${category}`;
-    const products = await fetch(url).then((res) => res.json());
+  const category = context.query.category;
+  const url = `https://fakestoreapi.com/products/category/${category}`;
+  const products = await fetch(url).then((res) => res.json());
 
-    if (!products.length) {
-        return {
-            notFound: true,
-        };
-    }
-
+  if (!products.length) {
     return {
-        props: {
-            products,
-            category,
-        },
+      notFound: true,
     };
+  }
+
+  return {
+    props: {
+      products,
+      category,
+    },
+  };
 };
